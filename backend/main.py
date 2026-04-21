a3fdf3a0648012b74844ce68ec983dc9ae5d2030
dhruv12raj-beep <dhruv12raj@gmail.com>
2026-04-22 00:50:45 +0530

    added frontend path

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://localhost:3000"
+        "https://aizenstore.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
