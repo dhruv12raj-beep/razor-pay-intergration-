56b757db50aa68811299fbbca464cffc10f64252
dhruv12raj-beep <dhruv12raj@gmail.com>
2026-04-22 01:08:06 +0530

    Fix production CORS

--- backend/main.py
+++ backend/main.py
@@ -14,8 +14,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "https://aizenstore.vercel.app"
+        "https://aizenstore.vercel.app",
+        "http://127.0.0.1:5500"
     ],
     allow_credentials=True,
     allow_methods=["*"],
